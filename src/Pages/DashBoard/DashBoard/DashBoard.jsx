2051d618b32a64beeaf706b191865ba565e7e52b
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-15 12:07:18 +0600

    made the private Route

--- src/Pages/DashBoard/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard/DashBoard.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Logo from '../../../Components/Logo/Logo';
 import { RiArchiveDrawerFill } from 'react-icons/ri';
 import { FaHome } from 'react-icons/fa';
+import { PiUserSwitchFill } from 'react-icons/pi';
+import Footer from '../../../Components/Footer/Footer';
 
 const DashBoard = () => {
     return (
-        <div className="max-w-7xl mx-auto bg-slate-100">
+        <div className="max-w-[1440px] mx-auto bg-slate-100">
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
 
                 {/* Main Content */}
                 <div className="drawer-content">
                     {/* Navbar */}
-                    <nav className="navbar w-full">
+                    <nav className="navbar w-full bg-linear-to-br from-indigo-200 to-purple-200 shadow-sm">
                         <label
                             htmlFor="my-drawer-4"
                             aria-label="open sidebar"
@@ -35,20 +37,37 @@ const DashBoard = () => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
 
-                    <div className="flex min-h-full flex-col bg-slate-900 text-slate-200 is-drawer-close:w-14 is-drawer-open:w-64">
+                    <div className="flex min-h-full flex-col py-5 bg-slate-900 text-slate-200 is-drawer-close:w-14 is-drawer-open:w-64">
                         <ul className="menu w-full grow p-2">
+
+                            {/* Home */}
                             <li>
                                 <button
-                                    className="hover:bg-slate-800 rounded-md is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                    data-tip="Homepage"
+                                    className="tooltip tooltip-right hover:bg-slate-800 rounded-md"
+                                    data-tip="Home Page"
                                 >
-                                    <FaHome className="is-drawer-close:hidden size-5" />Home Page
+                                    <FaHome className="size-6" />
+                                    <span className="is-drawer-close:hidden">Home Page</span>
                                 </button>
                             </li>
+
+                            {/* Manage Users */}
+                            <li>
+                                <button
+                                    className="tooltip tooltip-right hover:bg-slate-800 rounded-md"
+                                    data-tip="Manage Users"
+                                >
+                                    <PiUserSwitchFill className="size-6" />
+                                    <span className="is-drawer-close:hidden">Manage Users</span>
+                                </button>
+                            </li>
+
                         </ul>
                     </div>
+
                 </div>
             </div>
+            <Footer></Footer>
         </div>
 
     );
--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Children } from 'react';
+import useAuth from '../../Hooks/useAuth/useAuth';
+import { Navigate, useLocation } from 'react-router';
+import Spinner from '../../Components/Spinner/Spinner'
+
 
 const PrivateRoute = ({ children }) => {
-    return (
-        <div>
+    const { user, loading } = useAuth();
+    const location = useLocation();
 
-        </div>
-    );
+    //checking if we got the loading state true if yes then returning a spinner
+    if (loading) {
+        return <Spinner></Spinner>
+    }
+    //if the user does not exist taking him/her to login
+    if (!user) {
+        return <Navigate state={location.pathname} to={'/login'}></Navigate>
+    }
+    //if the user exist routing him to his intended path
+    return children;
 };
 
 export default PrivateRoute;
