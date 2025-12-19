7305cdb5f5c6caf35f168bf2b2fe8b99acac8f13
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-12-19 12:55:51 +0600

    only left is deployment

--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -6,16 +6,13 @@ import Spinner from '../../Components/Spinner/Spinner'
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
-    // const location = useLocation();
-    // console.log(location.state);
-
     //checking if we got the loading state true if yes then returning a spinner
     if (loading) {
         return <Spinner></Spinner>
     }
     //if the user does not exist taking him/her to login
     if (!user) {
-        return <Navigate state={location.pathname} to={'/login'}></Navigate>
+        return <Navigate to={'/login'}></Navigate>
     }
     //if the user exist routing him to his intended path
     return children;
